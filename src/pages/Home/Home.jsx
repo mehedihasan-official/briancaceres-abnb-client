3b121b3c8dc0bd0f9c7617698552d679f1ef4c82
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2025-06-16 11:39:19 +0600

    new update for the front page done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="container mx-auto  md:px-6 lg:px-8">
       {/* Hero Section */}
-      <div className="relative h-[500px] w-full overflow-hidden mb-12">
+      <div className="relative h-[700px] w-full overflow-hidden mb-12">
         {/* Background Image - Replace with your actual image path */}
         <div className="absolute inset-0">
           <img
@@ -26,7 +26,7 @@ const Home = () => {
         <div className="absolute inset-0 bg-black bg-opacity-10"></div>
         
         {/* Content */}
-        <div className="relative h-full flex flex-col items-start justify-center text-center text-white px-4">
+        <div className="relative h-full space-y-6 flex flex-col items-start justify-center text-center text-white px-4">
           <h1 className="text-5xl text-start md:text-5xl lg:text-6xl font-semibold mb-4">
             List your timeshare rentals
           </h1>
